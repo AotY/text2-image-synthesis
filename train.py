24e62753bcd1d8305509e41172504d180d8b2ef6
AotY <694731929@qq.com>
2018-10-29 17:43:09 +0800

    sh

--- train.py
+++ train.py
@@ -9,8 +9,10 @@
 """
 
 import torch
-import torch.nn
-
+import torch.nn as nn
+import torch.optim as optim
+from dataset import Text2ImageDataset
+from modul.gan import Generator, Discriminator
 
 lr = 0.0005
 batch_size = 64
@@ -19,11 +21,36 @@ epochs = 200
 
 device = torch.device('cuda')
 
+flowers_dataset_path = './data/flowers.hdf5'
+split = 0
+data_set = Text2ImageDataset(flowers_dataset_path, split=split)
+
+generator = nn.DataParallel(Generator(
+    image_size=64,
+    num_channels=3,
+    noise_size=100,
+    embedding_size=1024,
+    projected_embedding_size=128,
+    ngf=64
+))
+
+discriminator = nn.DataParallel(Discriminator(
+    image_size=64,
+    num_channels=3,
+    embedding_size=1024,
+    projected_embedding_size=128,
+    ndf=64,
+    b_size=128,
+    c_size=16
+))
+
+optimizerD = optim.Adam(discriminator.parameters(), lr=lr, betas=(0.9, 0.999))
+optimizerG = optim.Adam(generator.parameters(), lr=lr, betas(0,9, 0.999))
 
 def train():
     criterion = nn.BCELoss()
-    l2_loss = nn.MSELoss()
-    l1_loss = nn.L1Loss()
+    l2_criterion = nn.MSELoss()
+    l1_criterion = nn.L1Loss()
     iter = 0
 
     for epoch in range(epochs):
@@ -33,10 +60,12 @@ def train():
             right_images = sample['right_images'].float().to(device)
             right_embedded = sample['right_embedded'].float().to(device)
             wrong_images = sample['wrong_images'].float().to(device)
+            print('right_images shape: {}'.format(right_images.shape))
+            print('right_embedded shape: {}'.format(right_embedded.shape))
 
             real_labels = torch.ones(right_images.size(0), device=device)
             fake_lables = torch.zeros(right_images.size(0), device=device)
-            
+
             # ======== One sided label smoothing ==========
             # Helps preventing the discriminator from overpowering the
             # generator adding penalty when the discriminator is too confident
@@ -56,6 +85,7 @@ def train():
 
             noise = torch.randn(right_images(0), 100, device=device)
             noise = noise.view(noise.size(0), 100, 1, 1)
+            print('noise shape: {}'.format(noise.shape))
 
             fake_images = generator(right_embedded, noise)
             outputs, _ = discriminator(fake_images, right_embedded)
@@ -81,8 +111,8 @@ def train():
             activation_real = torch.mean(activation_real, 0)
 
             g_loss = criterion(outputs, real_labels) \
-                     + l2_coef * l2_loss(activation_fake, activation_real) \
-                     + l1_coef * l1_loss(fake_images, right_images)
+                     + l2_coef * l2_criterion(activation_fake, activation_real) \
+                     + l1_coef * l1_criterion(fake_images, right_images)
 
             g_loss.backward()
 
@@ -92,5 +122,5 @@ def train():
                 self.logger.log_iteration_gan(epoch,d_loss, g_loss, real_score, fake_score)
 
 
-            
-
+if __name__ == '__main__':
+    train()
---
+++ train.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# train.sh
+# Copyright (C) 2018 LeonTao
+#
+# Distributed under terms of the MIT license.
+#
+
+
+export CUDA_VISIBLE_DEVICES=4
+
+python train.py
+
